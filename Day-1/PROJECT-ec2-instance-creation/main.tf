7729c96214d28830b8afbd86a90e16f3654ec250
rishiapj <37538792+rishiapj@users.noreply.github.com>
2025-10-08 20:37:52 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-052064a798f08f0d3"  # Amazon Linux 2023 for us-east-1
+  ami           = "ami-0360c520857e3138f"  # Amazon Linux 2023 for us-east-1
     instance_type = "t2.micro"
 }
